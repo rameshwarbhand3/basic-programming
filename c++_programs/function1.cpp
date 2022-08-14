9ecbb830fba8fb19bda631e9eef43535c5d40da4
rameshwarbhand3 <rameshwarbhand3@gmail.com>
2022-08-15 00:19:09 +0530

    format function1.cpp file

--- c++_programs/function1.cpp
+++ c++_programs/function1.cpp
@@ -1,17 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int add(int a,int b){ 
-int c;
-c=a+b;
-return c;
+int add(int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
 }
-int main(){
-int a,b;
-cout<<"Enter the value of a"<<endl;
-cin>>a;
-cout<<"Enter the value of b"<<endl;
-cin>>b;
-cout<<"The adition of two number"<<add(a,b)<<endl;
-return(0);
+int main()
+{
+    int a, b;
+    cout << "Enter the value of a" << endl;
+    cin >> a;
+    cout << "Enter the value of b" << endl;
+    cin >> b;
+    cout << "The adition of two number" << add(a, b) << endl;
+    return (0);
 }
-
