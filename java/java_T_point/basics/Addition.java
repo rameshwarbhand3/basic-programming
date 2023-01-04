c6f91cc5d2faf23629b157835d2182627b3caa60
rameshwarbhand3 <rameshwarbhand3@gmail.com>
2023-01-04 11:50:28 +0530

    Basics Assignment

---
+++ java/java_T_point/basics/Addition.java
@@ -0,0 +1,10 @@
+package basics;
+
+ class Addition {
+    public static void main(String[] args){
+        int a =4;
+        int b =5;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
---
+++ java/java_T_point/basics/VariableDemo.java
@@ -0,0 +1,15 @@
+package basics;
+
+
+class VariableDemo {
+    static int m = 10;// static variable
+
+    void method() {
+        int n = 20;// local variable
+    }
+
+    public static void main(String args[]) {
+        int data = 50;// instance variable
+        System.out.println("Hello java");
+    }
+}
